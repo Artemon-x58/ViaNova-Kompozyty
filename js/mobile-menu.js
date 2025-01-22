ff633a1a064e971b5586c45c5e053e79bb809fe9
Artemon-x58 <artem1145819@gmail.com>
2025-01-23 00:05:36 +0100

    adaptive header and hero

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const burgerButton = document.querySelector(".btn-burger");
   const closeButton = document.querySelector(".btn-close");
   const navigationMenu = document.querySelector(".navigation");
+  const body = document.body;
 
   burgerButton.addEventListener("click", () => {
     navigationMenu.classList.add("active"); // Добавляем класс для показа меню
+    body.classList.add("no-scroll");
   });
 
   closeButton.addEventListener("click", () => {
     navigationMenu.classList.remove("active"); // Убираем класс для скрытия меню
+    body.classList.remove("no-scroll");
   });
 });
